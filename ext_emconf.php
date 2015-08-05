59015022c8f2d04227051650952fea46fca953d0
Christian Plattner <ccpp@gmx.at>
2015-08-05 11:33:53 +0200

    [WIP][TASK] Initial commit of Calendar list plugin
    Change-Id: Icafc22dec4cd9a3509cb9a9c115e9014c1ab5360
    Releases: master, 6.2

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+/***************************************************************
+ * Extension Manager/Repository config file for ext "list_CAL".
+ ***************************************************************/
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Web>List Calendar',
+	'description' => 'BE calendar for database-records',
+	'category' => 'module',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'author' => 'Christian Plattner',
+	'author_email' => 'ccpp@gmx.at',
+	'author_company' => '',
+	'version' => '0.1',
+	'_md5_values_when_last_written' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	),
+);
---
+++ mod1/conf.php
@@ -0,0 +1,7 @@
+<?php
+// DO NOT REMOVE OR CHANGE THESE 2 LINES:
+$MCONF['name'] = 'web_listcal';
+$MCONF['script'] = '_DISPATCH';
+$MCONF['access'] = 'user,group';
+$MLANG['default']['tabs_images']['tab'] = 'list_cal.gif';
+$MLANG['default']['ll_ref'] = 'LLL:EXT:list_cal/locallang_mod_web_listcal.xlf';
